d7c8fb1befc706a3172b8d9f2eea636ce82a5925
KangJiUng <woong236406@gmail.com>
2025-10-14 13:59:21 +0900

    feat: Route 도메인 생성

--- test/init_db.py
+++ test/init_db.py
@@ -2,6 +2,9 @@
 # poetry run python -m test.init_db
 from database import Base, engine
 from user.infra.db_models.user import User
+from report.infra.db_models.report import Report
+
+# from report.infra.db_models.report_comment import ReportComment
 
 print("Creating tables...")
 Base.metadata.create_all(bind=engine)
