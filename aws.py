9f7e343fcfef94b4aeb3416f6a302721764db51d
KangJiUng <woong236406@gmail.com>
2025-11-09 23:31:14 +0900

    fix: AWS S3 presigned URL 요청 시 서명 불일치 에러 해결

--- aws.py
+++ aws.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.config import Config
 from config import get_settings
 
 settings = get_settings()
@@ -8,4 +9,9 @@ s3_client = boto3.client(
     aws_access_key_id=settings.aws_access_key_id,
     aws_secret_access_key=settings.aws_secret_access_key,
     region_name=settings.aws_region,
+    endpoint_url=f"https://s3.{settings.aws_region}.amazonaws.com",
+    config=Config(
+        signature_version="s3v4",
+        s3={"addressing_style": "virtual"},
+    ),
 )
