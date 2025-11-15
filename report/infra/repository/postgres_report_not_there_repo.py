daaf36467b6480fbbe14fe617c2fbcf9e931b346
KangJiUng <woong236406@gmail.com>
2025-11-15 18:28:17 +0900

    fix: DB 중복 에러 처리 추가

--- report/infra/repository/postgres_report_not_there_repo.py
+++ report/infra/repository/postgres_report_not_there_repo.py
@@ -1,8 +1,10 @@
 import ulid
+from fastapi import HTTPException
 from database import SessionLocal
 from report.domain.report_not_there import ReportNotThere as NotThereVO
 from report.infra.db_models.report_not_there import ReportNotThere as NotThereDB
 from report.domain.repository.report_not_there_repo import ReportNotThereRepository
+
 
 class PostgresReportNotThereRepository(ReportNotThereRepository):
     def save(self, record: NotThereVO) -> NotThereVO:
@@ -14,7 +16,15 @@ class PostgresReportNotThereRepository(ReportNotThereRepository):
                 created_at=record.created_at,
             )
             db.add(db_record)
-            db.commit()
+            try:
+                db.commit()
+            except Exception as e:
+                db.rollback()
+                # 중복이면 400 에러로 리턴
+                if "uq_report_user" in str(e):
+                    raise HTTPException(status_code=400, detail="이미 눌렀습니다.")
+                raise e
+
             db.refresh(db_record)
             return NotThereVO.from_orm(db_record)
 
@@ -22,7 +32,9 @@ class PostgresReportNotThereRepository(ReportNotThereRepository):
         with SessionLocal() as db:
             exists = (
                 db.query(NotThereDB)
-                .filter(NotThereDB.report_id == report_id, NotThereDB.user_id == user_id)
+                .filter(
+                    NotThereDB.report_id == report_id, NotThereDB.user_id == user_id
+                )
                 .first()
             )
             return exists is not None
